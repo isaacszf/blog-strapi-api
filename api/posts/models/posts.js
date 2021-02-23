dba030bc143e4ba92bf48678c4cd7d25a94e379e
IkeSZ <isaacsouzafernandes1@hotmail.com>
2021-02-23 20:28:57 -0300

    Added life-cycle hooks

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -5,4 +5,18 @@
  * to customize this model
  */
 
-module.exports = {};
+const axios = require('axios');
+
+module.exports = {
+  afterCreate: async (result, data) => {
+    await axios.post(
+      'https://api.netlify.com/build_hooks/60358f36962c8215b4cd08a4'
+    );
+  },
+
+  afterUpdate: async (result, params, data) => {
+    await axios.post(
+      'https://api.netlify.com/build_hooks/60358f36962c8215b4cd08a4'
+    );
+  },
+};
