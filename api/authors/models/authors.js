6d2b7ae3fd73667af884938c951a875b562e5dac
IkeSZ <isaacsouzafernandes1@hotmail.com>
2021-02-24 20:00:31 -0300

    Added life-cycle hook to Authors

--- api/authors/models/authors.js
+++ api/authors/models/authors.js
@@ -5,4 +5,20 @@
  * to customize this model
  */
 
-module.exports = {};
+const axios = require('axios');
+
+module.exports = {
+  lifecycles: {
+    async afterCreate(result, data) {
+      axios.post(
+        'https://api.netlify.com/build_hooks/60358f36962c8215b4cd08a4'
+      );
+    },
+
+    async afterUpdate(result, params, data) {
+      axios.post(
+        'https://api.netlify.com/build_hooks/60358f36962c8215b4cd08a4'
+      );
+    },
+  },
+};
